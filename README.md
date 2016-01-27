176c873676d05bf371765eb1744ce20a19a0d9b9
ewnd9 <ewndnine@gmail.com>
2016-01-27 22:07:59 +0800

    refactor

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # universal-api
 
-[WIP] `superagent` wrapper intended to help writting libs for third-party api running both on server and browser via jsonp
+`superagent` wrapper intended to help writting libs for third-party api running both on server and browser via jsonp
 
 > Why?
 
 Because it sucks to have different libs on backend and frontend.
-
-> Why `superagent`?
-
-- node libs (you could use them through `browserify`, however `webpack` support is broken (https://github.com/sindresorhus/got/issues/127#issuecomment-171915766))
-  - [request](https://www.npmjs.com/package/request)
-  - [got](https://www.npmjs.com/package/got)
-
-- universal libs
-  - [superagent](https://www.npmjs.com/package/superagent)
-    - current choice
-  - [isomorphic-fetch](https://www.npmjs.com/package/isomorphic-fetch)
-    - no jsonp support
-  - [axios](https://www.npmjs.com/package/axios)
-    - no jsonp support and it won't be implemented (https://github.com/mzabriskie/axios/pull/15)
 
 ## Install
 
@@ -29,6 +15,10 @@ $ npm install universal-api --save
 ## Dependents
 
 - [`vk-universal-api`](https://www.npmjs.com/package/vk-universal-api)
+
+## Changelog
+
+- `v0.1.0` - breaking changes due to complete refactor
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,58 +1,93 @@
-var Promise = require('bluebird');
+import Promise from 'bluebird';
+import superagent from 'superagent';
+import isNode from 'detect-node';
 
-var request = require('superagent');
-var isNode = require('detect-node');
+function UniversalApi({ baseUrl = '', jsonp = false, query, body, headers, transformResponse }) {
+  this.baseUrl = baseUrl;
+  this.jsonp = !!jsonp && !isNode;
+  this.query = query;
+  this.body = body;
+  this.headers = headers;
+  this.transformResponse = transformResponse;
 
-export default function(baseUrl, options) {
-  options = options || {};
-
-  var verbose = options.verbose;
-  var api = {};
-
-  api.transformRequest = (method, params, isNode) => params;
-  api.transformResponse = (res, resolve, reject) => resolve(res);
-
-  api.jsonp = options.jsonp || false;
-
-  if (!isNode && api.jsonp) {
-    require('superagent-jsonp')(request);
+  if (this.jsonp) {
+    require('superagent-jsonp')(superagent);
   }
-
-  api.apiRequest = function(method, params, libParams) {
-    libParams = libParams || {};
-
-    return new Promise(function(resolve, reject) {
-      var url = baseUrl + method;
-
-      if (verbose) {
-        console.log(url);
-      }
-
-      var req = request.get(url);
-
-      if (options.transformRequest && libParams.transformRequest !== false) {
-        req = req.query(options.transformRequest(method, params || {}, isNode));
-      } else {
-        req = req.query(api.transformRequest(method, params || {}, isNode));
-      }
-
-      if (!isNode && api.jsonp) {
-        req = req.jsonp();
-      }
-
-      req.end(function(err, res) {
-        if (err) {
-          reject(err);
-        } else {
-          if (options.transformResponse && libParams.transformResponse !== false) {
-            options.transformResponse(res, resolve, reject);
-          } else {
-            api.transformResponse(res, resolve, reject);
-          }
-        }
-      });
-    });
-  };
-
-  return api;
 };
+
+const fnOrObject = input => {
+  if (typeof input === 'function') {
+    return input();
+  } else {
+    return input || {};
+  }
+};
+
+UniversalApi.prototype.request = function(method, url, query = {}, body = {}, headers = {}) {
+  const result = new Promise((resolve, reject) => {
+    const req = superagent(method, this.baseUrl + url);
+
+    if (this.jsonp) {
+      req.jsonp();
+    }
+
+    const requestQuery = {
+      ...fnOrObject(this.query),
+      ...query
+    };
+
+    req.query(requestQuery);
+
+    if (method === 'POST' || method === 'PUT') {
+      const requestBody = {
+        ...fnOrObject(this.body),
+        ...body
+      };
+
+      req.send(requestBody);
+    }
+
+    const requestHeaders = {
+      ...fnOrObject(this.headers),
+      ...headers
+    };
+
+    req.set(requestHeaders);
+
+    req.end((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.body);
+      }
+    });
+  });
+
+  if (this.transformResponse) {
+    return result.then(this.transformResponse);
+  } else {
+    return result;
+  }
+};
+
+UniversalApi.prototype.get = function(url, query = {}, headers = {}) {
+  return this.request('GET', url, query, null, headers);
+};
+
+UniversalApi.prototype.post = function(url, query = {}, body = {}, headers = {}) {
+  return this.request('POST', url, query, body, headers);
+};
+
+UniversalApi.prototype.put = function(url, query = {}, body = {}, headers = {}) {
+  return this.request('PUT', url, query, body, headers);
+};
+
+UniversalApi.prototype.delete = function(url, query = {}, headers = {}) {
+  return this.request('DELETE', url, query, null, headers);
+};
+
+UniversalApi.prototype.head = function(url, query = {}, headers = {}) {
+  return this.request('HEAD', url, query, null, headers);
+};
+
+export default UniversalApi;
